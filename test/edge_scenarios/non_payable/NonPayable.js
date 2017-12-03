cdb52c698161eb55ec6143fae8c5d84c6570acce
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2017-12-03 19:29:05 -0300

    Init project

---
+++ test/edge_scenarios/non_payable/NonPayable.js
@@ -0,0 +1,14 @@
+const NonPayable = artifacts.require('./NonPayable.sol')
+
+contract('Given a non payable contract', accounts => {
+
+  it('throws if someone tries to send it funds', async function () {
+    const nonPayable = await NonPayable.new()
+
+    try {
+      await nonPayable.sendTransaction({from: accounts[0], value: 1000, gasPrice: 0})
+    } catch(error) {
+      assert(error.message.search('invalid opcode') > 0);
+    }
+  })
+})
---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      port: 8545,
+      network_id: "*",
+      host: "localhost",
+      gas: 4600000
+    }
+  }
+}
