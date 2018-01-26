079c6ee9449e94443095a79a3d3c16c0864df8e2
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-01-26 12:31:47 -0300

    Fix non payable test

--- test/edge_scenarios/non_payable/NonPayable.js
+++ test/edge_scenarios/non_payable/NonPayable.js
@@ -2,14 +2,13 @@ const NonPayable = artifacts.require('./NonPayable.sol')
 
 contract('Given a non payable contract', accounts => {
 
-  it('throws if someone tries to send it funds', async function () {
+  it('reverts if someone tries to send it funds', async function () {
     const nonPayable = await NonPayable.new()
 
     try {
       await nonPayable.sendTransaction({ from: accounts[0], value: 1000, gasPrice: 0 })
       assert.fail();
     } catch(error) {
-      console.log(error)
       assert(error.message.search('revert') > 0);
     }
   })
